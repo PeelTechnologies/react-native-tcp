c67d90052b49f444daff0ae4501e937d14ebf0eb
Andy Prock <aprock@gmail.com>
2015-12-21 13:57:43 -0800

    add stubs for android implementation

---
+++ android/src/main/java/com/peel/react/TcpSockets.java
@@ -0,0 +1,132 @@
+/**
+ *  TcpSockets.java
+ *  react-native-tcp
+ *
+ *  Created by Andy Prock on 12/21/15.
+ */
+
+package com.peel.react;
+
+import android.content.Context;
+import android.net.wifi.WifiManager;
+import android.support.annotation.Nullable;
+import android.util.SparseArray;
+
+import com.facebook.common.logging.FLog;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.GuardedAsyncTask;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+
+import java.io.IOException;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * The NativeModule in charge of storing active {@link TcpSocketClient}s, and acting as an api layer.
+ */
+public final class TcpSockets extends ReactContextBaseJavaModule {
+    private static final String TAG = "TcpSockets";
+
+    private boolean mShuttingDown = false;
+
+    public TcpSockets(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    @Override
+    public String getName() {
+        return TAG;
+    }
+
+    @Override
+    public void initialize() {
+        mShuttingDown = false;
+    }
+
+    @Override
+    public void onCatalystInstanceDestroy() {
+        mShuttingDown = true;
+    }
+
+    /**
+     * Creates a {@link TcpSocketClient} with the given ID
+     */
+    @ReactMethod
+    public void createSocket(final Integer cId) {
+        new GuardedAsyncTask<Void, Void>(getReactApplicationContext()) {
+            @Override
+            protected void doInBackgroundGuarded(Void... params) {
+                FLog.e(TAG, "TcpSockets.createSocket unimplemented.");
+
+                WritableMap eventParams = Arguments.createMap();
+                eventParams.putString("event", "error");
+                eventParams.putString("data", "TcpSockets.createSocket unimplemented");
+
+                ReactContext reactContext = TcpSockets.this.getReactApplicationContext();
+                reactContext
+                        .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                        .emit("tcp-" + cId + "-event", eventParams);
+            }
+        }.execute();
+    }
+
+    @ReactMethod
+    public void connect(final Integer cId, final @Nullable String host, final Integer port, final ReadableMap options) {
+      new GuardedAsyncTask<Void, Void>(getReactApplicationContext()) {
+          @Override
+          protected void doInBackgroundGuarded(Void... params) {
+              FLog.e(TAG, "TcpSockets.connect unimplemented.");
+
+              WritableMap eventParams = Arguments.createMap();
+              eventParams.putString("event", "error");
+              eventParams.putString("data", "TcpSockets.connect unimplemented");
+
+              ReactContext reactContext = TcpSockets.this.getReactApplicationContext();
+              reactContext
+                      .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                      .emit("tcp-" + cId + "-event", eventParams);
+          }
+      }.execute();
+    }
+
+    @ReactMethod
+    public void write(final Integer cId, final String base64String, final boolean encoded, final Callback callback) {
+      new GuardedAsyncTask<Void, Void>(getReactApplicationContext()) {
+          @Override
+          protected void doInBackgroundGuarded(Void... params) {
+              FLog.e(TAG, "TcpSockets.write unimplemented.");
+              callback.invoke("unimplemented." + cId);
+          }
+      }.execute();
+    }
+
+    @ReactMethod
+    public void end(final Integer cId, final Callback callback) {
+      new GuardedAsyncTask<Void, Void>(getReactApplicationContext()) {
+          @Override
+          protected void doInBackgroundGuarded(Void... params) {
+              FLog.e(TAG, "TcpSockets.end unimplemented.");
+              callback.invoke("unimplemented." + cId);
+          }
+      }.execute();
+    }
+
+    @ReactMethod
+    public void destroy(final Integer cId, final Callback callback) {
+      new GuardedAsyncTask<Void, Void>(getReactApplicationContext()) {
+          @Override
+          protected void doInBackgroundGuarded(Void... params) {
+              FLog.e(TAG, "TcpSockets.destroy unimplemented.");
+              callback.invoke("unimplemented." + cId);
+          }
+      }.execute();
+    }
+}
---
+++ android/src/main/java/com/peel/react/TcpSocketsModule.java
@@ -0,0 +1,43 @@
+/**
+ *  TcpSocketsModule.java
+ *  react-native-tcp
+ *
+ *  Created by Andy Prock on 12/21/15.
+ */
+
+package com.peel.react;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public final class TcpSocketsModule implements ReactPackage {
+
+    @Override
+    public List<NativeModule> createNativeModules(
+            ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<NativeModule>();
+
+        modules.add(new TcpSockets(reactContext));
+
+        return modules;
+    }
+
+    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(
+            ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+}
