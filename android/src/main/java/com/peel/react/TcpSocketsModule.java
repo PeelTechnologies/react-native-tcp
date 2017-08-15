09cd1a7b23ce8ceb64d7332df4c109b3af039b04
Jacob Lewallen <jlewalle@gmail.com>
2017-08-15 11:34:31 -0700

    Remove obsolete method.
    https://github.com/itinance/react-native-fs/pull/324

--- android/src/main/java/com/peel/react/TcpSocketsModule.java
+++ android/src/main/java/com/peel/react/TcpSocketsModule.java
@@ -29,11 +29,6 @@ public final class TcpSocketsModule implements ReactPackage {
     }
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
-
-    @Override
     public List<ViewManager> createViewManagers(
             ReactApplicationContext reactContext) {
         return Collections.emptyList();
